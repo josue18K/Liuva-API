b9bbe0b3b53473426b55a7d3bd771dbdd5549e2e
JosueRamos1348033@ <josuern5593@gmail.com>
2026-08-12 19:47:58 -0500

    feat: publicar aplicacion web de Liuva

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app/{path?}', function () {
+    return response()->file(public_path('app/index.html'));
+})->where('path', '.*');
